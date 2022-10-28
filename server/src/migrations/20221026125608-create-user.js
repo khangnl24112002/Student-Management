f8b96aa39ae71a51443423f0e32150e9e37dc60d
Vox Tranaf Anh Kieejt <61832885+kietgula@users.noreply.github.com>
2022-10-28 19:42:01 +0700

    fix migrations

--- server/src/migrations/20221026125608-create-user.js
+++ server/src/migrations/20221026125608-create-user.js
@@ -15,14 +15,14 @@ module.exports = {
       password: {
         type: Sequelize.STRING
       },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
+      // createdAt: {
+      //   allowNull: false,
+      //   type: Sequelize.DATE
+      // },
+      // updatedAt: {
+      //   allowNull: false,
+      //   type: Sequelize.DATE
+      // }
     });
   },
   async down(queryInterface, Sequelize) {
