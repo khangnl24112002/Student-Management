cc6ba241c13b49cbfb2a80762a0c8d0f26d07ae1
NhatMinh <minhphan113@gmail.com>
2022-11-06 20:39:21 +0700

    Add index.js class route

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
+const { classRouter } = require("./classRoute");
 const studentRouter = require("./studentRoute");
 const { userRouter } = require("./userRoute");
-
 
 const rootRouter = express.Router();
 rootRouter.use("/users", userRouter);
 rootRouter.use("/students", studentRouter);
+rootRouter.use("/class", classRouter);
 
 module.exports = rootRouter;
-
